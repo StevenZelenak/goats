d17df67acd18489a1ed764dda25cd1903db16b32
Steven Zelenak <stevenmzelenak@gmail.com>
2020-05-11 18:56:18 -0500

    finished date branch

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.scss';
+import goatData from '../helpers/data/goatData';
 
 class App extends React.Component {
+  // outside render - anything that modifies state
+  componentDidMount() {
+    // this function works on load
+    const goats = goatData.getGoats();
+  }
+
   render() {
+    // inside render - anything we need to do to modify the user interface
     return (
       <div className="App">
-        <h2>INSIDE APP COMPONENT</h2>
-        <button className="btn btn-info">Goats</button>
+        <h2>Goats</h2>
+        <button className="btn btn-info">Tree</button>
       </div>
     );
   }
